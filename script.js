83c81ddb5992189a20fdbe653526d220e7cec056
rubiosppr <mateusd@alunos.utfpr.edu.br>
2025-07-02 19:36:52 -0300

    Final se deus quiser

--- script.js
+++ script.js
@@ -21,8 +21,11 @@ function numeroSecreto() {
     }
   }
 }
-function formulario() {
-  window.location.href = "formulário/index.html";
+function jogoSoma() {
+  let num1 = parseInt(prompt("Digite o primeiro número:"));
+  let num2 = parseInt(prompt("Digite o segundo número:"));
+  let resultado = num1 + num2;
+  alert(`A soma de ${num1} e ${num2} é: ${resultado}`);
 }
 
 function jogoMedia() {
