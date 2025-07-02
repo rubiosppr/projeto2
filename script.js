ed6238d7c129569b9be4b9931afa884678f58025
rubiosppr <mateusd@alunos.utfpr.edu.br>
2025-07-02 13:54:06 -0300

    Media Arrumado

--- script.js
+++ script.js
@@ -59,42 +59,32 @@ function numeroSecreto() {
       }
      }
     function jogoMedia() {
-      alert("Bem-vindo ao Jogo da Média!");
+      ﻿alert("Bem-vindo ao Jogo da Média!");
 
-      let quantidade = prompt("Quantos números você quer digitar?");
-      quantidade = Number(quantidade);
+let quantidade = prompt("Quantos números você quer digitar?");
+quantidade = Number(quantidade);
 
-      if (
-        quantidade <= 0 ||
-        quantidade === null ||
-        quantidade === "" ||
-        typeof quantidade !== "number"
-      ) {
-        alert(
-          "Quantidade inválida. Tente novamente com um número maior que zero."
-        );
-      } else {
-        let soma = 0;
+if (quantidade <= 0 || quantidade === null || quantidade === "" || typeof quantidade !== "number") {
+  alert("Quantidade inválida. Tente novamente com um número maior que zero.");
+} else {
+  let soma = 0;
 
-        for (let i = 1; i <= quantidade; i++) {
-          let entrada = prompt(`Digite o ${i}º número:`);
-          let numero = Number(entrada);
+  for (let i = 1; i <= quantidade; i++) {
+    let entrada = prompt(`Digite o ${i}º número:`);
+    let numero = Number(entrada);
 
-          if (entrada.trim() === "" || entrada === null || isNaN(numero)) {
-            alert("Valor inválido. Digite um número.");
-            i--;
-            continue;
-          }
+    if (entrada.trim() === "" || entrada === null || isNaN(numero)) {
+      alert("Valor inválido. Digite um número.");
+      i--;
+      continue;
+    }
 
-          soma += numero;
-        }
+    soma += numero;
+  }
 
-        let media = soma / quantidade;
+  let media = soma / quantidade;
 
-        alert(
-          `A média calculada entre os ${quantidade} números digitados é: ${media.toFixed(
-            2
-          )}`
-        );
-      }
+
+  alert(`A média calculada entre os ${quantidade} números digitados é: ${media.toFixed(2)}`);
+}
     }
